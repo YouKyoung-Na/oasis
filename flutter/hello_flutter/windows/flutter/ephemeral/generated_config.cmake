a8759bf6b1c12d9771dd937c7ea64c9d60c5684d
CBHY <codebyhy@gmail.com>
2022-07-21 01:08:54 +0900

    Final Application Develop Commit

---
+++ flutter/hello_flutter/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter"
+  "FLUTTER_TARGET=C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\kkhhy\\Desktop\\flutter\\hello_flutter\\.dart_tool\\package_config.json"
+)
